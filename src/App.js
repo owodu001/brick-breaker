a2aa55f3ff256b76fbd9fd16150854341d6af1a7
Michelle Owodunni <michelle.owodunni@gmail.com>
2020-01-22 14:13:14 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Paddle from "./components/paddle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Paddle />
+    </div>
+  );
+}
---
+++ src/components/paddle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Paddle.css";
+
+export default function Paddle() {
+  return <div className="paddle" />;
+}
